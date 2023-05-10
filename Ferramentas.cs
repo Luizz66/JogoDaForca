001f8fed171ec354a7cea5f37e340b47e588d9c0
Luiz <luizgustavoka66@gmail.com>
2023-05-10 19:07:12 -0300

    Ferramentas

---
+++ Ferramentas.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca
+{
+    public static class Ferramentas
+    {
+        public static void ColorLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+        public static void ColorBack(string text, ConsoleColor color)
+        {
+            Console.BackgroundColor = color;
+            Console.Write(text);
+            Console.ResetColor();
+        }
+
+
+        public static string RemoverAcentos(string texto)
+        {
+            string textoNormalizado = texto.Normalize(NormalizationForm.FormD);
+            StringBuilder resultado = new StringBuilder();
+
+            foreach (char letra in textoNormalizado)
+            {
+                if (CharUnicodeInfo.GetUnicodeCategory(letra) != UnicodeCategory.NonSpacingMark)
+                {
+                    resultado.Append(letra);
+                }
+            }
+
+            return resultado.ToString().Normalize(NormalizationForm.FormC);
+        }
+    }
+}
--- Play.cs
+++ Play.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using static Colorir.Color;
-using static Ferramentas.Remove;
+using static JogoDaForca.Ferramentas;
 
 namespace JogoDaForca
 {
